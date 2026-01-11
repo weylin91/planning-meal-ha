3d93829e2863788452887bf088ff5de6124fd359
Chevallier Romain <romain.chevallier-ext@aphp.fr>
2026-01-12 00:30:53 +0100

    correction fichier init

--- custom_components/meal_ha/__init__.py
+++ custom_components/meal_ha/__init__.py
@@ -10,23 +10,13 @@ from .db import init_db
 from .food_library import FoodLibrary
 
 async def async_setup(hass, config):
-            # Déclare la plateforme sensor pour la liste des ingrédients
-            hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
-        # Service pour lister les ingrédients
-        async def handle_list_foods(call):
-            food_lib = FoodLibrary(hass)
-            foods = food_lib.list_foods()
-            if foods:
-                msg = "Ingrédients : " + ", ".join([f"{fid}: {name}" for fid, name in foods])
-            else:
-                msg = "Aucun ingrédient trouvé."
-            hass.components.logger.info(msg)
-
-        hass.services.async_register(DOMAIN, "list_foods", handle_list_foods, schema=None)
     """Initialisation du composant."""
     hass.data[DOMAIN] = {}
     # Création/initialisation de la base de données
     init_db(hass)
+
+    # Déclare la plateforme sensor pour la liste des ingrédients
+    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
 
     # Service pour ajouter un ingrédient
     async def handle_add_food(call):
@@ -37,4 +27,17 @@ async def async_setup(hass, config):
         food_lib.add_food(name)
 
     hass.services.async_register(DOMAIN, "add_food", handle_add_food, schema=None)
+
+    # Service pour lister les ingrédients (logs)
+    async def handle_list_foods(call):
+        food_lib = FoodLibrary(hass)
+        foods = food_lib.list_foods()
+        if foods:
+            msg = "Ingrédients : " + ", ".join([f"{fid}: {name}" for fid, name in foods])
+        else:
+            msg = "Aucun ingrédient trouvé."
+        hass.components.logger.info(msg)
+
+    hass.services.async_register(DOMAIN, "list_foods", handle_list_foods, schema=None)
+
     return True
